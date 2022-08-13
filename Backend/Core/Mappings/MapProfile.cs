a3aac48ab3542c59aeeaed742aebc2a16780975e
Maks Michalski <mamich1998@gmail.com>
2022-08-13 08:18:27 +0200

    Dodanie mapowania do projektu

---
+++ Backend/Core/Mappings/MapProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Core.Dtos.Attack;
+using Core.Dtos.Position;
+using Core.Dtos.Ship;
+using Core.Dtos.User;
+using Domain.Entities;
+
+namespace Core.Mappings
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<AttackEntity, AttackDto>().ReverseMap();
+            CreateMap<PositionEntity, PositionDto>().ReverseMap();
+            CreateMap<ShipEntity, ShipDto>().ReverseMap();
+            CreateMap<ShipEntity, ShipInputDto>().ReverseMap();
+            CreateMap<UserEntity, UserDto>().ReverseMap();
+            CreateMap<UserEntity, UserInputDto>().ReverseMap();
+        }
+    }
+}
